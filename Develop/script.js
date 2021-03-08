3bed42a19ec9a555137a5be1d3f5e7e06a552845
BarbaraSheaKenny <b.kenny58@gmail.com>
2021-03-08 14:27:09 -0500

    updated README and cleaned up class names

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,10 @@
-
-var textarea8 = document.getElementById("8");
-var timeBlock8 = textarea8.dataset["number"];
-
-
 //display current day at the top of the page
 
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-// functions to update row color
 
+// functions to update row color
 
 function TimeChecker () { 
 
@@ -18,7 +13,7 @@ function TimeChecker () {
   
   var timeInt = Number.parseInt(actualNow);
 
-  $(".form-control").each ( function(){
+  $(".input").each ( function(){
 
     var timeBlockHour = parseInt($(this).attr("id"));
     console.log(timeBlockHour);
@@ -50,12 +45,11 @@ console.log(this);
 
 //save input
 
-
 $(".saveBtn").on("click", function(){
 
   var hours = $(this).parent().attr("id");
   console.log(hours);
-  var input = $(this).siblings(".col-10").children(".form-control").val();
+  var input = $(this).siblings(".inputDiv").children(".input").val();
   console.log(input);
 
   localStorage.setItem(hours, input);
@@ -64,19 +58,16 @@ $(".saveBtn").on("click", function(){
 
 //get input
 
-$("#hour-8 .col-10 #8").val(localStorage.getItem("hour-8"));
-$("#hour-8 .col-10 #9").val(localStorage.getItem("hour-9"));
-$("#hour-10 .col-10 #10").val(localStorage.getItem("hour-10"));
-$("#hour-11 .col-10 #11").val(localStorage.getItem("hour-11"));
-$("#hour-12 .col-10 #12").val(localStorage.getItem("hour-12"));
-$("#hour-13 .col-10 #13").val(localStorage.getItem("hour-13"));
-$("#hour-14 .col-10 #14").val(localStorage.getItem("hour-14"));
-$("#hour-15 .col-10 #15").val(localStorage.getItem("hour-15"));
-$("#hour-16 .col-10 #16").val(localStorage.getItem("hour-16"));
-$("#hour-17 .col-10 #17").val(localStorage.getItem("hour-17"));
-
-
-
+$("#hour-8 .inputDiv #8").val(localStorage.getItem("hour-8"));
+$("#hour-8 .inputDiv #9").val(localStorage.getItem("hour-9"));
+$("#hour-10 .inputDiv #10").val(localStorage.getItem("hour-10"));
+$("#hour-11 .inputDiv #11").val(localStorage.getItem("hour-11"));
+$("#hour-12 .inputDiv #12").val(localStorage.getItem("hour-12"));
+$("#hour-13 .inputDiv #13").val(localStorage.getItem("hour-13"));
+$("#hour-14 .inputDiv #14").val(localStorage.getItem("hour-14"));
+$("#hour-15 .inputDiv #15").val(localStorage.getItem("hour-15"));
+$("#hour-16 .inputDiv #16").val(localStorage.getItem("hour-16"));
+$("#hour-17 .inputDiv #17").val(localStorage.getItem("hour-17"));
 
 
 TimeChecker();
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# calendar
+# Calendar
+
+A simple calendar application that allows the user to add events/tasks for each hour of the day. Each hour is color coded based on the current time. The events/tasks added by the user will be available to the user after a page refresh. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery and Moment.js.
+
+# Built with
+
+-HTML
+-CSS
+-Bootstrap
+-Javascript
+-JQuery
+-Moment.js
+
+# Link
+
+
+
+# Preview
